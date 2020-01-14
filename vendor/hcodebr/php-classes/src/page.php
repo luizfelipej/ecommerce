28d1e9cb341640dfded43f41fdf815e110bce541
luizfelipej <luizfelipejesus94@gmail.com>
2020-01-14 13:44:26 -0200

    Tela de login

--- vendor/hcodebr/php-classes/src/page.php
+++ vendor/hcodebr/php-classes/src/page.php
@@ -8,7 +8,11 @@ class Page {
 
 	private $tpl;
 	private $options = [];
-	private $defaults = ["data"=>[]];
+	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
+		"data"=>[]
+	];
 
 
 	public function __construct($opts = array(), $tpl_dir = "/views/") {
@@ -27,7 +31,7 @@ class Page {
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if ($this->options["header"] === true) $this->tpl->draw("header");
 	}
 
 	private function setData($data = array()) {
@@ -47,7 +51,7 @@ class Page {
 
 	public function __destruct() {
 
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
 	}
 
