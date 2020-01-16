8355984174728aaa8aee790848d23818c1cba997
luizfelipej <luizfelipejesus94@gmail.com>
2020-01-16 14:09:29 -0200

    Site categorias

--- index.php
+++ index.php
@@ -287,6 +287,18 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 	exit;
 });
 
+$app->get("/admin/categories/:idcategory/products", function($idcategory){
+	User::verifyLogin();
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$page = new PageAdmin();
+	$page->setTpl("categories-products", [
+		'category'=>$category->getValues(),
+		'productsRelated'=>$category->getProducts(),
+		'productsNotRelated'=>$category->getProducts(false)
+	]);
+});
+
 
 
 $app->run();
